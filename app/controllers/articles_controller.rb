03038122e699cf87e3ed5d04e956d7018730db53
OPR-A-RUBin <opr.a.ruby@gmail.com>
2022-07-26 23:10:30 +0400

    Re-Coding from old to new patern

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,23 @@
 class ArticlesController < ApplicationController
 
   def new
+    @article = Article.new
   end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+    # render plain: params[:article].inspect
   end  
 
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
 
