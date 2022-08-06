8731e710b862856471d7b8457e4b9cba1ad1626d
OPR-A-RUBin <opr.a.ruby@gmail.com>
2022-08-06 22:06:29 +0400

    Add view onse article
    http://localhost:3000/articles/2

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,10 @@ class ArticlesController < ApplicationController
     # render plain: params[:article].inspect
   end  
 
+  def show
+    @article = Article.find(params[:id])
+  end
+  
   private
     def article_params
       params.require(:article).permit(:title, :text)
