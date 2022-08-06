9a5878800e095cea3380cb279188a4f38e538b57
OPR-A-RUBin <opr.a.ruby@gmail.com>
2022-08-06 22:21:30 +0400

    Add display output articles List

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
 
+  def index
+    @articles = Article.all
+  end
+
   def new
     @article = Article.new
   end
@@ -18,7 +22,7 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
-  
+
   private
     def article_params
       params.require(:article).permit(:title, :text)
